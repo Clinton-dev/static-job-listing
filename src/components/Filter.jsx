a6e2c18705118b87a10b6db5b69248006b15d1de
Clinton wambugu <clintonwambugu@gmail.com>
2022-09-27 15:54:31 +0300

    Add activestates

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -4,12 +4,12 @@ import React, { useEffect } from 'react';
 function Filter({ setLanguage }) {
 
     return (
-        <div className='container w-5/6 mx-auto -mt-4 mb-12 bg-white flex justify-between py-2 px-4 md:mb-0'>
-            <div className='flex space-x-4'>
+        <div className='container w-5/6 mx-auto -mt-4 mb-12 bg-white rounded-md flex justify-between py-2 px-4 md:mb-0'>
+            <div className='flex flex-wrap space-x-4'>
                 {/* language */}
                 <div className='flex bg-LightGrayCyanFb '>
-                    <div className='text-DesDarkCyan p-2'>language</div>
-                    <button className='p-2 bg-DesDarkCyan'><img src="src/assets/images/close-icon.svg" alt="close icon" /></button>
+                    <div className='text-DesDarkCyan p-2'>All</div>
+                    <button className='p-2 bg-DesDarkCyan hover:bg-VeryDarkGrayCyan'><img src="src/assets/images/close-icon.svg" alt="close icon" /></button>
                 </div>
             </div>
             <div>
