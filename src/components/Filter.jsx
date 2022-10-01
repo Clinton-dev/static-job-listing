2622ba5d25a1ab34a3be6aad1bf1a74e9af2b8f7
Clinton wambugu <clintonwambugu@gmail.com>
2022-10-01 22:28:43 +0300

    dynamically Remove language from filter component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -7,11 +7,11 @@ function Filter({ setFilteredLanguages, filteredLanguages }) {
         <div className='container w-5/6 mx-auto -mt-4 mb-12 bg-white rounded-md flex justify-between py-2 px-4 md:mb-0'>
             <div className='flex flex-wrap space-x-4'>
                 {/* language */}
-                {filteredLanguages.map((language) => {
+                {filteredLanguages.map((language, index) => {
                     return (
                         <div className='flex bg-LightGrayCyanFb '>
                             <div className='text-DesDarkCyan p-2'>{language}</div>
-                            <button className='p-2 bg-DesDarkCyan hover:bg-VeryDarkGrayCyan'><img src="src/assets/images/close-icon.svg" alt="close icon" /></button>
+                            <button className='p-2 bg-DesDarkCyan hover:bg-VeryDarkGrayCyan' onClick={(e) => setFilteredLanguages(filteredLanguages.filter(lang => lang != language))}><img src="src/assets/images/close-icon.svg" alt="close icon" /></button>
                         </div>
                     )
                 })}
